f4e0ae6b00437cc499c94af9003bb413fad91fe9
Sascha Egerer <info@sascha-egerer.de>
2018-11-28 23:30:14 +0100

    [BUGFIX] Update reflections to be compatible with 0.10 interfaces

--- src/Reflection/RepositoryFindOneByMethodReflection.php
+++ src/Reflection/RepositoryFindOneByMethodReflection.php
@@ -5,6 +5,8 @@ namespace SaschaEgerer\PhpstanTypo3\Reflection;
 
 use PHPStan\Broker\Broker;
 use PHPStan\Reflection\ClassReflection;
+use PHPStan\Reflection\ClassMemberReflection;
+use PHPStan\Reflection\FunctionVariant;
 use PHPStan\Reflection\MethodReflection;
 use PHPStan\Type\ObjectType;
 use PHPStan\Type\Type;
@@ -55,7 +57,7 @@ class RepositoryFindOneByMethodReflection implements MethodReflection
         return true;
     }
 
-    public function getPrototype(): MethodReflection
+    public function getPrototype(): ClassMemberReflection
     {
         return $this;
     }
@@ -96,5 +98,19 @@ class RepositoryFindOneByMethodReflection implements MethodReflection
     public function getReturnType(): Type
     {
         return TypeCombinator::addNull(new ObjectType($this->getModelName()));
+    }
+
+    /**
+     * @return \PHPStan\Reflection\ParametersAcceptor[]
+     */
+    public function getVariants(): array
+    {
+        return [
+            new FunctionVariant(
+                $this->getParameters(),
+                $this->isVariadic(),
+                $this->getReturnType()
+            ),
+        ];
     }
 }
