2b98c21138ea3a9eddc3d322b75a1ed50ec0246c
Markus Klösges <markus@computercamp.at>
2020-04-05 11:39:35 +0200

    Use FQCN instead of use statements to allow compatibility with TYPO 8.7

--- src/Type/ContextDynamicReturnTypeExtension.php
+++ src/Type/ContextDynamicReturnTypeExtension.php
@@ -9,20 +9,13 @@ use PHPStan\Reflection\ParametersAcceptorSelector;
 use PHPStan\Type\DynamicMethodReturnTypeExtension;
 use PHPStan\Type\ObjectType;
 use PHPStan\Type\Type;
-use TYPO3\CMS\Core\Context\Context;
-use TYPO3\CMS\Core\Context\DateTimeAspect;
-use TYPO3\CMS\Core\Context\LanguageAspect;
-use TYPO3\CMS\Core\Context\TypoScriptAspect;
-use TYPO3\CMS\Core\Context\UserAspect;
-use TYPO3\CMS\Core\Context\VisibilityAspect;
-use TYPO3\CMS\Core\Context\WorkspaceAspect;
 
 class ContextDynamicReturnTypeExtension implements DynamicMethodReturnTypeExtension
 {
 
 	public function getClass(): string
 	{
-		return Context::class;
+		return \TYPO3\CMS\Core\Context\Context::class;
 	}
 
 	public function isMethodSupported(
@@ -45,18 +38,18 @@ class ContextDynamicReturnTypeExtension implements DynamicMethodReturnTypeExtens
 
 		switch ($arg->value) {
 			case 'date':
-				return new ObjectType(DateTimeAspect::class);
+				return new ObjectType(\TYPO3\CMS\Core\Context\DateTimeAspect::class);
 			case 'visibility':
-				return new ObjectType(VisibilityAspect::class);
+				return new ObjectType(\TYPO3\CMS\Core\Context\VisibilityAspect::class);
 			case 'backend.user':
 			case 'frontend.user':
-				return new ObjectType(UserAspect::class);
+				return new ObjectType(\TYPO3\CMS\Core\Context\UserAspect::class);
 			case 'workspace':
-				return new ObjectType(WorkspaceAspect::class);
+				return new ObjectType(\TYPO3\CMS\Core\Context\WorkspaceAspect::class);
 			case 'language':
-				return new ObjectType(LanguageAspect::class);
+				return new ObjectType(\TYPO3\CMS\Core\Context\LanguageAspect::class);
 			case 'typoscript':
-				return new ObjectType(TypoScriptAspect::class);
+				return new ObjectType(\TYPO3\CMS\Core\Context\TypoScriptAspect::class);
 			default:
 				return ParametersAcceptorSelector::selectSingle($methodReflection->getVariants())->getReturnType();
 		}
