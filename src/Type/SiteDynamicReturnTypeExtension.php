8108487969322788236473a4a2a6f5eb36a2e99b
Thomas Lüder <t.lueder@nets.de>
2022-08-12 00:26:29 +0200

    Added site object value types

--- src/Type/SiteDynamicReturnTypeExtension.php
+++ src/Type/SiteDynamicReturnTypeExtension.php
@@ -6,9 +6,26 @@ use PhpParser\Node\Expr\MethodCall;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\MethodReflection;
 use PHPStan\Reflection\ParametersAcceptorSelector;
+use PHPStan\Type\Accessory\AccessoryLiteralStringType;
+use PHPStan\Type\Accessory\AccessoryNonEmptyStringType;
+use PHPStan\Type\Accessory\AccessoryNonFalsyStringType;
+use PHPStan\Type\Accessory\AccessoryNumericStringType;
+use PHPStan\Type\Accessory\NonEmptyArrayType;
+use PHPStan\Type\ArrayType;
+use PHPStan\Type\BenevolentUnionType;
+use PHPStan\Type\BooleanType;
+use PHPStan\Type\Constant\ConstantBooleanType;
 use PHPStan\Type\DynamicMethodReturnTypeExtension;
-use PHPStan\Type\ObjectType;
+use PHPStan\Type\FloatType;
+use PHPStan\Type\IntegerRangeType;
+use PHPStan\Type\IntegerType;
+use PHPStan\Type\IntersectionType;
+use PHPStan\Type\IterableType;
+use PHPStan\Type\MixedType;
+use PHPStan\Type\StringType;
 use PHPStan\Type\Type;
+use PHPStan\Type\TypeCombinator;
+use PHPStan\Type\UnionType;
 
 class SiteDynamicReturnTypeExtension implements DynamicMethodReturnTypeExtension
 {
@@ -42,7 +59,101 @@ class SiteDynamicReturnTypeExtension implements DynamicMethodReturnTypeExtension
 		}
 
 		if (isset($this->siteApiGetAttributeMapping[$argument->value->value])) {
-			return new ObjectType($this->siteApiGetAttributeMapping[$argument->value->value]);
+			switch (strtolower($this->siteApiGetAttributeMapping[$argument->value->value])) {
+				case 'int':
+				case 'integer':
+					return new IntegerType();
+
+				case 'positive-int':
+					return IntegerRangeType::fromInterval(1, null);
+
+				case 'negative-int':
+					return IntegerRangeType::fromInterval(null, -1);
+
+				case 'string':
+					return new StringType();
+
+				case 'literal-string':
+					return new IntersectionType([new StringType(), new AccessoryLiteralStringType()]);
+
+				case 'array-key':
+					return new BenevolentUnionType([new IntegerType(), new StringType()]);
+
+				case 'number':
+					return new UnionType([new IntegerType(), new FloatType()]);
+
+				case 'numeric':
+					return new UnionType([
+						new IntegerType(),
+						new FloatType(),
+						new IntersectionType([
+							new StringType(),
+							new AccessoryNumericStringType(),
+						]),
+					]);
+
+				case 'numeric-string':
+					return new IntersectionType([
+						new StringType(),
+						new AccessoryNumericStringType(),
+					]);
+
+				case 'non-empty-string':
+					return new IntersectionType([
+						new StringType(),
+						new AccessoryNonEmptyStringType(),
+					]);
+
+				case 'truthy-string':
+				case 'non-falsy-string':
+					return new IntersectionType([
+						new StringType(),
+						new AccessoryNonFalsyStringType(),
+					]);
+
+				case 'bool':
+					return new BooleanType();
+
+				case 'boolean':
+					return new BooleanType();
+
+				case 'true':
+					return new ConstantBooleanType(true);
+
+				case 'false':
+					return new ConstantBooleanType(false);
+
+				case 'float':
+					return new FloatType();
+
+				case 'double':
+					return new FloatType();
+
+				case 'array':
+				case 'associative-array':
+					return new ArrayType(new MixedType(), new MixedType());
+
+				case 'non-empty-array':
+					return TypeCombinator::intersect(
+						new ArrayType(new MixedType(), new MixedType()),
+						new NonEmptyArrayType(),
+					);
+
+				case 'iterable':
+					return new IterableType(new MixedType(), new MixedType());
+
+				case 'mixed':
+					return new MixedType(true);
+
+				case 'list':
+					return new ArrayType(new IntegerType(), new MixedType());
+
+				case 'non-empty-list':
+					return TypeCombinator::intersect(
+						new ArrayType(new IntegerType(), new MixedType()),
+						new NonEmptyArrayType(),
+					);
+			}
 		}
 
 		return ParametersAcceptorSelector::selectSingle($methodReflection->getVariants())->getReturnType();
