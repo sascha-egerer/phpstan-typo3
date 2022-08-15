df8c48ac90b4ccd0b24acef52567f1b6b83b5eb3
Thomas Lüder <t.lueder@nets.de>
2022-08-15 17:52:35 +0200

    Make attributes of Request Api configurable (#51)
    Co-authored-by: Sascha Egerer <sascha.egerer@flowd.de>

---
+++ src/Type/RequestDynamicReturnTypeExtension.php
@@ -0,0 +1,58 @@
+<?php declare(strict_types = 1);
+
+namespace SaschaEgerer\PhpstanTypo3\Type;
+
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Reflection\ParametersAcceptorSelector;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
+
+class RequestDynamicReturnTypeExtension implements DynamicMethodReturnTypeExtension
+{
+
+	/** @var array<string, string> */
+	private $requestApiGetAttributeMapping;
+
+	/**
+	 * @param array<string, string> $requestApiGetAttributeMapping
+	 */
+	public function __construct(array $requestApiGetAttributeMapping)
+	{
+		$this->requestApiGetAttributeMapping = $requestApiGetAttributeMapping;
+	}
+
+	public function getClass(): string
+	{
+		return \Psr\Http\Message\ServerRequestInterface::class;
+	}
+
+	public function getTypeFromMethodCall(
+		MethodReflection $methodReflection,
+		MethodCall $methodCall,
+		Scope $scope
+	): Type
+	{
+		$argument = $methodCall->getArgs()[0] ?? null;
+
+		if ($argument === null || !($argument->value instanceof \PhpParser\Node\Scalar\String_)) {
+			return ParametersAcceptorSelector::selectSingle($methodReflection->getVariants())->getReturnType();
+		}
+
+		if (isset($this->requestApiGetAttributeMapping[$argument->value->value])) {
+			return new ObjectType($this->requestApiGetAttributeMapping[$argument->value->value]);
+		}
+
+		return ParametersAcceptorSelector::selectSingle($methodReflection->getVariants())->getReturnType();
+	}
+
+	public function isMethodSupported(
+		MethodReflection $methodReflection
+	): bool
+	{
+		return $methodReflection->getName() === 'getAttribute';
+	}
+
+}
