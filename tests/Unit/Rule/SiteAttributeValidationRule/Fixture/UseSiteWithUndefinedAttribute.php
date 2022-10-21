067c9c2a31a871c310c6e261b2c2e5505c760f2e
Sebastian Schreiber <me@schreibersebastian.de>
2022-10-21 15:51:56 +0200

    TASK: Add test for SiteAttributeValidationRule

---
+++ tests/Unit/Rule/SiteAttributeValidationRule/Fixture/UseSiteWithUndefinedAttribute.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types = 1);
+
+namespace SaschaEgerer\PhpstanTypo3\Tests\Unit\Rule\SiteAttributeValidationRule\Fixture;
+
+use TYPO3\CMS\Core\Site\Entity\Site;
+
+final class UseSiteWithUndefinedAttribute
+{
+
+	/** @var Site */
+	private $site;
+
+	public function __construct(Site $site)
+	{
+		$this->site = $site;
+	}
+
+	public function someMethod(): void
+	{
+		$this->site->getAttribute('foo');
+	}
+
+}
