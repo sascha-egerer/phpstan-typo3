5ab6870805f29b30f563517fd3c190b96bbe0cd8
Sascha Egerer <sascha.egerer@flowd.de>
2022-10-24 13:59:34 +0200

    [BUGFIX] Make QueryResultToArrayDynamicReturnTypeExtension work with CompoundType
    Resolves #103

--- src/Type/QueryResultToArrayDynamicReturnTypeExtension.php
+++ src/Type/QueryResultToArrayDynamicReturnTypeExtension.php
@@ -9,9 +9,11 @@ use PHPStan\Type\ArrayType;
 use PHPStan\Type\DynamicMethodReturnTypeExtension;
 use PHPStan\Type\ErrorType;
 use PHPStan\Type\Generic\GenericObjectType;
+use PHPStan\Type\Generic\TemplateType;
 use PHPStan\Type\IntegerType;
 use PHPStan\Type\ObjectType;
 use PHPStan\Type\Type;
+use PHPStan\Type\TypeTraverser;
 use SaschaEgerer\PhpstanTypo3\Helpers\Typo3ClassNamingUtilityTrait;
 use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
 
@@ -38,12 +40,14 @@ class QueryResultToArrayDynamicReturnTypeExtension implements DynamicMethodRetur
 		Scope $scope
 	): Type
 	{
-		$classReflection = $scope->getClassReflection();
+		$resultType = $this->getGenericTypes(
+			$scope->getType($methodCall->var)
+		)[0] ?? null;
 
-		$resultType = $scope->getType($methodCall->var);
 		if ($resultType instanceof GenericObjectType) {
 			$modelType = $resultType->getTypes();
 		} else {
+			$classReflection = $scope->getClassReflection();
 			if ($classReflection === null) {
 				return new ErrorType();
 			}
@@ -56,6 +60,34 @@ class QueryResultToArrayDynamicReturnTypeExtension implements DynamicMethodRetur
 		}
 
 		return new ArrayType(new IntegerType(), $modelType[0]);
+	}
+
+	/**
+	 * @return GenericObjectType[]
+	 */
+	private function getGenericTypes(Type $baseType): array
+	{
+		$genericObjectTypes = [];
+		TypeTraverser::map($baseType, static function (Type $type, callable $traverse) use (&$genericObjectTypes): Type {
+			if ($type instanceof GenericObjectType) {
+				$resolvedType =	TypeTraverser::map($type, static function (Type $type, callable $traverse): Type {
+					if ($type instanceof TemplateType) {
+						return $traverse($type->getBound());
+					}
+					return $traverse($type);
+				});
+				if (!$resolvedType instanceof GenericObjectType) {
+					throw new \PHPStan\ShouldNotHappenException();
+				}
+				$genericObjectTypes[] = $resolvedType;
+				$traverse($type);
+				return $type;
+			}
+			$traverse($type);
+			return $type;
+		});
+
+		return $genericObjectTypes;
 	}
 
 }
