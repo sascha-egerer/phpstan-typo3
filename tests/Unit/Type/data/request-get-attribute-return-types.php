bfb02864cba8c6f0657ec17c55821edb4e07cf7c
Elias Häußler <e.haeussler@familie-redlich.de>
2023-02-21 12:34:56 +0100

    [BUGFIX] Include default value passed to `$request->getAttribute()`
    Whenever the `$request->getAttribute()` method is called, it is
    possible to provide a default value in case the given argument is null.
    Thus, the default value should be part of the return type.

--- tests/Unit/Type/data/request-get-attribute-return-types.php
+++ tests/Unit/Type/data/request-get-attribute-return-types.php
@@ -4,6 +4,7 @@ namespace RequestGetAttributeReturnTypes;
 
 use Psr\Http\Message\ServerRequestInterface;
 use TYPO3\CMS\Core\Http\NormalizedParams;
+use TYPO3\CMS\Core\Site\Entity\NullSite;
 use TYPO3\CMS\Core\Site\Entity\Site;
 use TYPO3\CMS\Core\Site\Entity\SiteLanguage;
 use function PHPStan\Testing\assertType;
@@ -21,6 +22,7 @@ class MyRequest
 		);
 		assertType(SiteLanguage::class . '|null', $request->getAttribute('language'));
 		assertType(Site::class . '|null', $request->getAttribute('site'));
+		assertType(NullSite::class . '|' . Site::class, $request->getAttribute('site', new NullSite()));
 		assertType(NormalizedParams::class . '|null', $request->getAttribute('normalizedParams'));
 		assertType('1|2|4|8|16|null', $request->getAttribute('applicationType'));
 		assertType('FlowdGmbh\\MyProject\\Http\\MyAttribute|null', $request->getAttribute('myCustomAttribute'));
