7890bdc08ea275a932a114ea09f60dcbf9a3f5b6
Sebastian Schreiber <me@schreibersebastian.de>
2023-05-10 10:21:39 +0200

    TASK: Add dynamic return type extension for getIndpEnv
    Resolves: #125

---
+++ src/Type/GeneralUtilityGetIndpEnvDynamicReturnTypeExtension.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types = 1);
+
+namespace SaschaEgerer\PhpstanTypo3\Type;
+
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\StaticCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\ArrayType;
+use PHPStan\Type\BooleanType;
+use PHPStan\Type\Constant\ConstantStringType;
+use PHPStan\Type\DynamicStaticMethodReturnTypeExtension;
+use PHPStan\Type\StringType;
+use PHPStan\Type\UnionType;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class GeneralUtilityGetIndpEnvDynamicReturnTypeExtension implements DynamicStaticMethodReturnTypeExtension
+{
+
+	public function getClass(): string
+	{
+		return GeneralUtility::class;
+	}
+
+	public function isStaticMethodSupported(MethodReflection $methodReflection): bool
+	{
+		return $methodReflection->getName() === 'getIndpEnv';
+	}
+
+	public function getTypeFromStaticMethodCall(MethodReflection $methodReflection, StaticCall $methodCall, Scope $scope): ?\PHPStan\Type\Type
+	{
+		$firstArgument = $methodCall->args[0];
+
+		if (!$firstArgument instanceof Arg) {
+			return null;
+		}
+
+		$argumentType = $scope->getType($firstArgument->value);
+
+		if (!$argumentType instanceof ConstantStringType) {
+			return null;
+		}
+
+		if ($argumentType->getValue() === '_ARRAY') {
+			return new ArrayType(new StringType(), new UnionType([new StringType(), new BooleanType()]));
+		}
+
+		if (in_array($argumentType->getValue(), ['TYPO3_SSL', 'TYPO3_PROXY', 'TYPO3_REV_PROXY'], true)) {
+			return new BooleanType();
+		}
+
+		return new StringType();
+	}
+
+}
---
+++ tests/Unit/Type/GeneralUtilityGetIndpEnvDynamicReturnTypeExtension/data/GeneralUtilityGetIndpEnvTest.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types = 1);
+
+namespace SaschaEgerer\PhpstanTypo3\Tests\Unit\Type\GeneralUtilityGetIndpEnvDynamicReturnTypeExtension\data;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use function PHPStan\Testing\assertType;
+
+final class GeneralUtilityGetIndpEnvTest
+{
+
+	public function getScriptName(): void
+	{
+		$value = GeneralUtility::getIndpEnv('SCRIPT_NAME');
+		assertType('string', $value);
+	}
+
+	public function getSsl(): void
+	{
+		$value = GeneralUtility::getIndpEnv('TYPO3_SSL');
+		assertType('bool', $value);
+	}
+
+	public function getTypo3Proxy(): void
+	{
+		$value = GeneralUtility::getIndpEnv('TYPO3_PROXY');
+		assertType('bool', $value);
+	}
+
+	public function getArray(): void
+	{
+		$value = GeneralUtility::getIndpEnv('_ARRAY');
+		assertType('array<string, bool|string>', $value);
+	}
+
+}
