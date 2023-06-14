85a754989baf47bbc76b4fa1bc12f2977ba4852c
Alexander Schnitzler <git@alexanderschnitzler.de>
2023-06-14 19:45:06 +0200

    [FEATURE] Add dynamic return type extension for DateTimeAspect::get()

---
+++ tests/Unit/Type/DateTimeAspectGetDynamicReturnTypeExtension/DateTimeAspectGetDynamicReturnTypeExtensionTest.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types = 1);
+
+namespace SaschaEgerer\PhpstanTypo3\Tests\Unit\Type\DateTimeAspectGetDynamicReturnTypeExtension;
+
+use PHPStan\Testing\TypeInferenceTestCase;
+
+final class DateTimeAspectGetDynamicReturnTypeExtensionTest extends TypeInferenceTestCase
+{
+
+	/**
+	 *
+	 * @return iterable<mixed>
+	 */
+	public function dataFileAsserts(): iterable
+	{
+		yield from $this->gatherAssertTypes(__DIR__ . '/data/datetime-aspect-get-return-types.php');
+	}
+
+	/**
+	 * @dataProvider dataFileAsserts
+	 * @param string $assertType
+	 * @param string $file
+	 * @param mixed ...$args
+	 */
+	public function testFileAsserts(
+		string $assertType,
+		string $file,
+		...$args
+	): void
+	{
+		$this->assertFileAsserts($assertType, $file, ...$args);
+	}
+
+	public static function getAdditionalConfigFiles(): array
+	{
+		return [
+			__DIR__ . '/../../../../extension.neon',
+		];
+	}
+
+}
