d3fd0e6190aea17d9e7ad7632ce8d8b4f493cf5f
Sebastian Schreiber <me@schreibersebastian.de>
2024-02-06 23:48:35 +0100

    [TASK] Add test for PrototypeServiceDefinitionChecker

---
+++ tests/Unit/Fixtures/NonPrototypeClass.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types = 1);
+
+namespace SaschaEgerer\PhpstanTypo3\Tests\Unit\Fixtures;
+
+final class NonPrototypeClass
+{
+
+	public function __construct(string $requiredString)
+	{
+	}
+
+}
---
+++ tests/Unit/Service/PrototypeServiceDefinitionCheckerTest.php
@@ -0,0 +1,72 @@
+<?php declare(strict_types = 1);
+
+namespace SaschaEgerer\PhpstanTypo3\Tests\Unit\Service;
+
+use PhpParser\BuilderFactory;
+use PhpParser\Node\Expr\StaticCall;
+use PHPUnit\Framework\TestCase;
+use SaschaEgerer\PhpstanTypo3\Service\PrototypeServiceDefinitionChecker;
+use SaschaEgerer\PhpstanTypo3\Service\ServiceDefinition;
+use SaschaEgerer\PhpstanTypo3\Tests\Unit\Fixtures\NonPrototypeClass;
+use SaschaEgerer\PhpstanTypo3\Tests\Unit\Fixtures\PrototypeClass;
+
+final class PrototypeServiceDefinitionCheckerTest extends TestCase
+{
+
+	private PrototypeServiceDefinitionChecker $subject;
+
+	public static function provideNonPrototypes(): \Generator
+	{
+		$builderFactory = new BuilderFactory();
+		$prototypeClass = $builderFactory->classConstFetch(PrototypeClass::class, 'class');
+		$nonPrototypeClass = $builderFactory->classConstFetch(NonPrototypeClass::class, 'class');
+
+		yield 'Service definition has tags' => [
+			$builderFactory->staticCall('Foo', 'foo', [$prototypeClass]),
+			new ServiceDefinition('foo', 'bar', false, false, null, false, false, true),
+		];
+
+		yield 'Service definition has method calls' => [
+			$builderFactory->staticCall('Foo', 'foo', [$prototypeClass]),
+			new ServiceDefinition('foo', 'bar', false, false, null, false, true, false),
+		];
+
+		yield 'Service definition has non prototype class' => [
+			$builderFactory->staticCall('Foo', 'foo', [$nonPrototypeClass]),
+			new ServiceDefinition('foo', 'bar', false, false, null, false, false, false),
+		];
+	}
+
+	public static function providePrototypes(): \Generator
+	{
+		$builderFactory = new BuilderFactory();
+		$prototypeClass = $builderFactory->classConstFetch(PrototypeClass::class, 'class');
+
+		yield 'Service definition has no tags, no method calls and class has no required constructor arguments' => [
+			$builderFactory->staticCall('Foo', 'foo', [$prototypeClass]),
+			new ServiceDefinition('foo', 'bar', false, false, null, false, false, false),
+		];
+	}
+
+	protected function setUp(): void
+	{
+		$this->subject = new PrototypeServiceDefinitionChecker();
+	}
+
+	/**
+	 * @dataProvider providePrototypes
+	 */
+	public function testIsPrototypeIsTrue(StaticCall $node, ServiceDefinition $serviceDefinition): void
+	{
+		self::assertTrue($this->subject->isPrototype($serviceDefinition, $node));
+	}
+
+	/**
+	 * @dataProvider provideNonPrototypes
+	 */
+	public function testIsPrototypeIsFalse(StaticCall $node, ServiceDefinition $serviceDefinition): void
+	{
+			self::assertFalse($this->subject->isPrototype($serviceDefinition, $node));
+	}
+
+}
