6188f32ac85fa99556ed728c5251fa20c1f32963
Sebastian Schreiber <me@schreibersebastian.de>
2024-02-07 17:14:12 +0100

    [TASK] Remove realpath resolution

--- src/Service/ServiceDefinitionFileException.php
+++ src/Service/ServiceDefinitionFileException.php
@@ -7,14 +7,14 @@ final class ServiceDefinitionFileException extends \InvalidArgumentException
 
 	public static function notFound(string $file): self
 	{
-		$message = sprintf('File "%s" does not exist', realpath($file));
+		$message = sprintf('File "%s" does not exist', $file);
 
 		return new self($message);
 	}
 
 	public static function parseError(string $file): self
 	{
-		$message = sprintf('File "%s" could not be parsed correctly', realpath($file));
+		$message = sprintf('File "%s" could not be parsed correctly', $file);
 
 		return new self($message);
 	}
