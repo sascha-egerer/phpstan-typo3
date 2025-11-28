6ac44f90513f7c52fc0a7a0464ceccb7bf83cfd2
cs <cs@amedick-sommer.de>
2025-11-28 21:29:16 +0100

    [CLEANUP] Update return type in ServiceDefinitionFileException methods
    🔧 Changed return type from `self` to `ServiceDefinitionFileException` for clarity.

--- src/Service/ServiceDefinitionFileException.php
+++ src/Service/ServiceDefinitionFileException.php
@@ -5,14 +5,14 @@ namespace SaschaEgerer\PhpstanTypo3\Service;
 final class ServiceDefinitionFileException extends \InvalidArgumentException
 {
 
-	public static function notFound(string $file): self
+	public static function notFound(string $file): ServiceDefinitionFileException
 	{
 		$message = sprintf('File "%s" does not exist', $file);
 
 		return new self($message);
 	}
 
-	public static function parseError(string $file): self
+	public static function parseError(string $file): ServiceDefinitionFileException
 	{
 		$message = sprintf('File "%s" could not be parsed correctly', $file);
 
