3374491d1d7f5a67329f50e75683d64f4cc2e9a2
cs <cs@amedick-sommer.de>
2025-12-03 17:24:56 +0100

    [CLEANUP] Rather set return type to self
    For GH CoPilot means that using the FQCN is unnecessary

--- src/Service/ServiceDefinitionFileException.php
+++ src/Service/ServiceDefinitionFileException.php
@@ -5,14 +5,14 @@ namespace SaschaEgerer\PhpstanTypo3\Service;
 final class ServiceDefinitionFileException extends \InvalidArgumentException
 {
 
-	public static function notFound(string $file): \SaschaEgerer\PhpstanTypo3\Service\ServiceDefinitionFileException
+	public static function notFound(string $file): self
 	{
 		$message = sprintf('File "%s" does not exist', $file);
 
 		return new self($message);
 	}
 
-	public static function parseError(string $file): \SaschaEgerer\PhpstanTypo3\Service\ServiceDefinitionFileException
+	public static function parseError(string $file): self
 	{
 		$message = sprintf('File "%s" could not be parsed correctly', $file);
 
