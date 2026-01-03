1706cbb3623206ffdf19895be4e090d33a8a8c0a
Sascha Egerer <sascha.egerer@flowd.de>
2026-01-03 20:55:24 +0100

    Add rector and configuration

---
+++ rector.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+	->withPaths([
+		__DIR__ . '/src',
+		__DIR__ . '/tests',
+	])
+	->withPHPStanConfigs([
+		__DIR__ . '/phpstan.neon',
+	])
+	->withPreparedSets(
+		deadCode: true,
+		codeQuality: true,
+		codingStyle: true,
+		typeDeclarations: true,
+		privatization: true,
+		naming: true,
+		instanceOf: true,
+		earlyReturn: true,
+		rectorPreset: true,
+		phpunitCodeQuality: true,
+	);
